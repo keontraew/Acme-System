455dd6650fd4294e79f92185ec18d7738d1d4850
Keontrae Williams <wkeontrae@gmail.com>
2025-09-01 23:52:20 -0400

    Added dave

--- acmesystem/src/ver1/HelloWorld.java
+++ acmesystem/src/ver1/HelloWorld.java
@@ -3,10 +3,8 @@ package ver1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		System.out.println("Hello from Eclipse");
-		
+		System.out.println("Dave is working locally on his branch");
 
 	}
 
